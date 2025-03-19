734d8e07811db10eb8877db4066f41dd6f1a75c9
Mike Salvatore <mike.s.salvatore@gmail.com>
2025-03-19 03:55:14 -0400

    Fix hook creation

--- phone_caddy/phone_caddy.py
+++ phone_caddy/phone_caddy.py
@@ -21,6 +21,10 @@ PHONE_Y: Final[float] = 8
 PHONE_Z: Final[float] = 115
 
 HEADBOARD_THICKNESS: Final[float] = 30
+
+HOOK_Z: Final[float] = 30
+HOOK_THICKNESS: Final[float] = 2.5
+
 WALL_THICKNESS: Final[float] = 2
 OPPOSING_WALLS: Final[float] = 2 * WALL_THICKNESS
 FILLET: Final[float] = 2
@@ -37,17 +41,19 @@ def phone_caddy():
 
 
 def hook():
-    hook_z = 10
-    hook_thickness = 2
     hook_fillet = 1.25
 
-    l1 = Line((0, hook_z + hook_thickness), (0, 0))
-    l2 = Line(l1 @ 1, (-HEADBOARD_THICKNESS, 0))
-    l3 = Line(l2 @ 1, (-HEADBOARD_THICKNESS, hook_thickness + hook_fillet))
-    l4 = RadiusArc(l3 @ 1, (-HEADBOARD_THICKNESS + hook_fillet, hook_thickness), -hook_fillet)
-    l5 = Line(l4 @ 1, (-HEADBOARD_THICKNESS, hook_thickness))
-    l6 = Line(l5 @ 1, (-hook_thickness, hook_thickness))
-    l7 = Line(l6 @ 1, (-hook_thickness, hook_z + hook_thickness))
+    l1 = Line((0, HOOK_Z + HOOK_THICKNESS), (0, 0))
+    l2 = Line(l1 @ 1, (-(HEADBOARD_THICKNESS + HOOK_THICKNESS), 0))
+    l3 = Line(l2 @ 1, (-(HEADBOARD_THICKNESS + HOOK_THICKNESS), HOOK_THICKNESS + hook_fillet))
+    l4 = RadiusArc(
+        l3 @ 1,
+        (-(HEADBOARD_THICKNESS + HOOK_THICKNESS - hook_fillet), HOOK_THICKNESS),
+        -hook_fillet,
+    )
+    l5 = Line(l4 @ 1, (-(HEADBOARD_THICKNESS + HOOK_THICKNESS), HOOK_THICKNESS))
+    l6 = Line(l5 @ 1, (-HOOK_THICKNESS, HOOK_THICKNESS))
+    l7 = Line(l6 @ 1, (-HOOK_THICKNESS, HOOK_Z + HOOK_THICKNESS))
     l8 = Line(l7 @ 1, l1 @ 0)
 
     hook_2d = make_face([l1, l2, l3, l4, l5, l6, l7, l8])
